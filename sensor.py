bcba63d2952410e5290cc18e7d3ede7034f0ac1e
renji-pingla <renji@pingalasoftware.com>
2022-12-07 12:06:41 +0530

    addded led

--- sensor.py
+++ sensor.py
@@ -1,9 +1,13 @@
 from gpiozero import MotionSensor
-
+from gpiozero import LED
+white_led=LED(17)
 mvs= MotionSensor(4)
-
+white_led.off()
+print("start")
 while True:
-	mvs.wait_for_motion()
-	print("Motion detected")
-	mvs.wait_for_no_motion()
-	print("No motion detected")
+        mvs.wait_for_motion()
+        print("Motion detected")
+        white_led.on()
+        mvs.wait_for_no_motion()
+        white_led.off()
+        print("No motion detected")
