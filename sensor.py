df0d84c786f0c06be63d4701241e845170915dbd
renji-pingla <renji@pingalasoftware.com>
2022-12-07 16:31:10 +0530

    url call

--- sensor.py
+++ sensor.py
@@ -1,13 +1,33 @@
 from gpiozero import MotionSensor
 from gpiozero import LED
 white_led=LED(17)
+import requests
+url1 = 'http://localhost:3001/detected' 
+url2 = 'http://localhost:3001/notdetected' 
 mvs= MotionSensor(4)
 white_led.off()
 print("start")
 while True:
         mvs.wait_for_motion()
         print("Motion detected")
+        r1=requests.post(url1)     
+        print(r1)     
         white_led.on()
         mvs.wait_for_no_motion()
         white_led.off()
         print("No motion detected")
+        r2=requests.post(url2)     
+        print(r2) 
+
+# def motion():
+#     print("Motion detected")
+#     r=requests.post(url)     
+#     print(r)     
+# # motion()
+# def nomotion():
+#     print("no Motion detected")
+#     r=requests.post(url)     
+#     print(r)     
+# nomotion()
+ 
+ 
